8ef3e6b79a8119d8acf73ebcee61d1f3d28735fa
pfranza <pfranza@gmail.com>
2014-04-24 15:26:09 -0400

    adding gwt to the pom

---
+++ src/main/java/com/peterfranza/stackserver/ui/client/StackServerEntryPoint.java
@@ -0,0 +1,13 @@
+package com.peterfranza.stackserver.ui.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class StackServerEntryPoint implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		Window.alert("Loaded");
+	}
+
+}
